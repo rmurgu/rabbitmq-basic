1731081df3a62d91775c91ffe7e0bb558d24699f
rmurgu <no-reply>
2020-03-27 18:01:18 +0200

    Add an example with a durable queue with messages that are persisted to disk and consumers that takes 1 task at a time

---
+++ src/main/java/workqueue/NewTask.java
@@ -0,0 +1,41 @@
+package workqueue;
+
+import com.rabbitmq.client.Channel;
+import com.rabbitmq.client.Connection;
+import com.rabbitmq.client.ConnectionFactory;
+import com.rabbitmq.client.MessageProperties;
+
+/**
+ * Publisher with a durable queue and persistent messages
+ */
+
+public class NewTask {
+
+    private static final String TASK_QUEUE_NAME = "task_queue";
+
+    public static void main(String[] args) throws Exception {
+        ConnectionFactory factory = new ConnectionFactory();
+        factory.setHost("localhost");
+        try (Connection connection = factory.newConnection();
+             Channel channel = connection.createChannel()) {
+            channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
+
+            String message = ".";
+            int count = 0;
+
+            while (count < 100) {
+                String toSend = "Message " + count + ": " + message;
+
+                channel.basicPublish("", TASK_QUEUE_NAME,
+                        MessageProperties.PERSISTENT_TEXT_PLAIN,
+                        toSend.getBytes("UTF-8"));
+                System.out.println(" [x] Sent '" + toSend + "'");
+
+                message = message + ".";
+                count++;
+                Thread.sleep(1000);
+            }
+        }
+    }
+
+}
--- src/main/java/workqueue/Worker.java
+++ src/main/java/workqueue/Worker.java
@@ -1,28 +1,33 @@
-package helloworld;
+package workqueue;
 
 import com.rabbitmq.client.Channel;
 import com.rabbitmq.client.Connection;
 import com.rabbitmq.client.ConnectionFactory;
 import com.rabbitmq.client.DeliverCallback;
 
-public class Consumer {
+/**
+ * Consumer that takes 1 task at a time
+ */
 
-    private final static String QUEUE_NAME = "hello";
+public class Worker {
+
+    private static final String TASK_QUEUE_NAME = "task_queue";
 
     public static void main(String[] args) throws Exception {
-
         ConnectionFactory factory = new ConnectionFactory();
         factory.setHost("localhost");
+        final Connection connection = factory.newConnection();
+        final Channel channel = connection.createChannel();
 
-        Connection connection = factory.newConnection();
-        Channel channel = connection.createChannel();
-
-        channel.queueDeclare(QUEUE_NAME, false, false, false, null);
-
+        channel.queueDeclare(TASK_QUEUE_NAME, true, false, false, null);
         System.out.println(" [*] Waiting for messages. To exit press CTRL+C");
+
+        // Option for consumers to take 1 task at a time
+        channel.basicQos(1);
 
         DeliverCallback deliverCallback = (consumerTag, delivery) -> {
             String message = new String(delivery.getBody(), "UTF-8");
+
             System.out.println(" [x] Received '" + message + "'");
             try {
                 doWork(message);
@@ -31,8 +36,7 @@ public class Consumer {
                 channel.basicAck(delivery.getEnvelope().getDeliveryTag(), false);
             }
         };
-        boolean autoAck = false;
-        channel.basicConsume(QUEUE_NAME, false, deliverCallback, consumerTag -> {});
+        channel.basicConsume(TASK_QUEUE_NAME, false, deliverCallback, consumerTag -> {});
     }
 
     private static void doWork(String task) {
